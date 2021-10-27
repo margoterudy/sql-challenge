6d38e1d434945f93cc0dbb86b70fbe7b2e5ec8f9
Margot <margoterudy@gmail.com>
2021-10-27 10:46:27 -0500

    margot wed

--- EmployeeSQL/QuickDBD-export.sql
+++ EmployeeSQL/QuickDBD-export.sql
@@ -1,4 +1,4 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
